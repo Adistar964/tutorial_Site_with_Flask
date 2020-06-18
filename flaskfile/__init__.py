212c56558438497cb69fa79271c1383aaa8f9c3c
unknown <adistar964@gmail.com>
2020-06-18 21:03:16 +0300

    a change

--- flaskfile/__init__.py
+++ flaskfile/__init__.py
@@ -2,10 +2,14 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+import os
+import dotenv
+
+dotenv.load_dotenv()
 
 app = Flask(__name__, static_url_path='/static')
 app.secret_key = 'fc6285fe00c01600d6b88b6c368eb1'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
